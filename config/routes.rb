fcc1a7b0dccccc6de21d2ba6575d5a934e68e178
Nagisa Kawashima <ec2-user@ip-172-31-41-87.ap-northeast-1.compute.internal>
2022-11-11 05:47:30 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # get 'students/index'
   # get 'students/show'
   # get 'students/edit'
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items
   end
-  adminというフォルダは自身で作る必要がある
+  
 
 
   scope module: :school do
@@ -52,3 +53,6 @@ end
 
 # module（scope module）は
 # 「Controller#Action」のみカスタマイズしたい場合に使用します。
+
+
+# adminというフォルダは自身で作る必要があ
